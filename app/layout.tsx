f7a621ca5807c50bec129068eb3bdd277b666668
johnnybuildsyo <johnnybuilds@gmx.com>
2024-10-28 21:21:52 -0500

    Add Clerk integration and form submission handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
-import { ThemeProvider } from "@/components/theme-provider";
-import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { ClerkProvider } from "@clerk/nextjs"
+import { ThemeProvider } from "@/components/theme-provider"
+import type { Metadata } from "next"
+import localFont from "next/font/local"
 import { Rethink_Sans } from "next/font/google"
-import "./globals.css";
+import "./globals.css"
 
 const displayFont = Rethink_Sans({
   subsets: ["latin"],
@@ -14,37 +15,32 @@ const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
-});
+})
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
-});
+})
 
 export const metadata: Metadata = {
   title: "Johnny Builds",
   description: "Johnny builds web stuff for people in public",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${displayFont.variable} antialiased font-display`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${geistSans.variable} ${geistMono.variable} ${displayFont.variable} antialiased font-display`}>
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,28 @@
-import { RequestFormDialog } from "@/components/request-form-dialog";
+import { Button } from "@/components/ui/button"
+import { PlusIcon } from "lucide-react"
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-4 row-start-2 justify-center items-center font-display">
+    <div className="min-h-screen flex flex-col font-display">
+      <main className="flex flex-col grow gap-4 row-start-2 justify-center items-center font-display py-12">
         <h1 className="text-7xl font-extrabold">Johnny Builds</h1>
         <p className="text-xl text-center">I build web stuff for people in public</p>
-        <RequestFormDialog />
+        <Link href="/request">
+          <Button className="flex items-center gap-2 font-display">
+            <PlusIcon size={16} />
+            Make a Request
+          </Button>
+        </Link>
       </main>
+      <footer className="p-8">
+        <h3 className="pb-4 font-bold">Posts</h3>
+        <ul>
+          <li>
+            <a href="https://medium.com/@johnnybuilds/how-johnny-builds-new-web-projects-51f32b39bc20">How Johnny Builds New Web Projects</a>
+          </li>
+        </ul>
+      </footer>
     </div>
-  );
+  )
 }
