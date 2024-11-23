e6720a2b7e8fd7586bead9dd5374a78b45c9818a
johnnybuildsyo <johnnybuilds@gmx.com>
2024-11-23 12:59:17 -0600

    remove unused import

--- components/project-dau-widget.tsx
+++ components/project-dau-widget.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Legend, ResponsiveContainer, Scatter, Tooltip } from "recharts"
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Legend, ResponsiveContainer, Tooltip } from "recharts"
 import { ChartContainer } from "@/components/ui/chart"
 import { DAUData } from "@/app/_types"
 
